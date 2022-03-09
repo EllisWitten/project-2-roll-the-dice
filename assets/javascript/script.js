357244543f66c30b6f8594c4cee176e8cc226fc6
EllisWitten <e.witten.2001@gmail.com>
2022-03-09 21:02:12 +0000

    add copmuter roll text and display the computers roll

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,16 +17,29 @@ function playGame(){
     let cnum1 = Math.floor(Math.random()*6 + 1);
     let cnum2 = Math.floor(Math.random()*6 + 1);
 
-    document.getElementById('command-text').textContent = `Your first dice was ${pnum1} and your second dice was ${pnum2}`;
+    setTimeout(function(){changeCommandText(`Your first dice was ${pnum1} and your second dice was ${pnum2}`)}, 1000);
 
     let playerDiceValue = caluclateDiceValue(pnum1,pnum2); 
-    console.log(playerDiceValue);
     let computerDiceValue = caluclateDiceValue(cnum1,cnum2);
-    console.log(computerDiceValue);
+
+    setTimeout(function(){changePlayerRoll(playerDiceValue)}, 1000);
+    setTimeout(function(){changeCommandText('Lets see what the computer gets...')}, 5000);
+    setTimeout(function(){changeCommandText(`Computers first dice was ${cnum1} and Computers second dice was ${cnum2}`)}, 1000);
+    setTimeout(function(){changeComputerRoll(computerDiceValue)}, 1000);
+
 }
 function caluclateDiceValue(num1,num2) {
     let sum = num1 + num2;
     return sum;
+}
+function changeCommandText(string){
+    document.getElementById('command-text').textContent = string;
+}
+function changePlayerRoll(num){
+    document.getElementById('player-roll').textContent = num;
+}
+function changeComputerRoll(num){
+    document.getElementById('computer-roll').textContent = num;
 }
 function compareDiceValues() {
 
