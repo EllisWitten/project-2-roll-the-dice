e820b5ccd1eb6c340bf0298c26a6e9da5be874be
EllisWitten <e.witten.2001@gmail.com>
2022-03-11 20:56:12 +0000

    i have began fixing the bug which was that everytime the game restarted it created new event listeners and started over incrementin event listeners and looped the game thousands of times, i have done this by only having one event listner but i have had to returcture my code completely

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -1,11 +1,3 @@
-startGame();
-let wins = 0;
-let loses = 0;
-let draws = 0;
-function startGame(){
-    changeCommandText('Roll your dice!');
-    changeComputerRoll(0);
-    changePlayerRoll(0);
 document.addEventListener('DOMContentLoaded', function () {
     let buttons = document.getElementsByTagName('img');
 
@@ -13,25 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
         button.addEventListener('click', function() {
             if (this.getAttribute('data-type') === 'player-roll') {
                 let dataType = this.getAttribute('data-type');
-                playGame(dataType);
-            } else {
-                console.log('button clicked does not equal player roll')
+                rollPlayersDice(dataType);
+            } else if (this.getAttribute('data-type') === 'computer-roll' && (document.getElementById('command-text').innerHTML === 'Roll the Computers dice')){
+                let dataType = this.getAttribute('data-type');
+                rollComputersDice(dataType);
+            } else{
+                console.log('no values are met');
+                console.log (this.getAttribute('data-type'));
+                console.log(document.getElementById('command-text').innerHTML);
             }
         })
     }
 })
-}
-
-function playGame(dataType) {
+function rollPlayersDice(dataType) {
     let pnum1 = Math.floor(Math.random() * 6 + 1);
     let pnum2 = Math.floor(Math.random() * 6 + 1);
 
     let commandText = document.getElementById('command-text').textContent;
-    let playGame1 = (dataType === 'player-roll') && (commandText === 'Roll your dice!');
+    let playerRoll = (dataType === 'player-roll') && (commandText === 'Roll your dice!');
 
-
-    console.log(playGame1);
-    if (playGame1) {
+    if (playerRoll) {
         startPlayerRoll(pnum1, pnum2);
     } else{
         console.log('button clicked does not equal player roll or command text does not equal Roll your dice!')
@@ -45,46 +38,30 @@ function startPlayerRoll(num1, num2) {
     changeCommandText(`your first dice was ${num1} and your second dice was ${num2}`);
     changeCommandText('Roll the Computers dice');
     changePlayerRoll(sum);
-    waitForPlayerClick();
 }
-
-function waitForPlayerClick() {
-    console.log('waitForPlayerClick has started');
-    let buttons = document.getElementsByTagName('img');
-
-    for (let button of buttons) {
-        button.addEventListener('click', function () {
-            if (this.getAttribute('data-type') == 'computer-roll') {
-                let dataType = this.getAttribute('data-type');
-                startComputerRoll(dataType);
-            } else {
-                console.log('button clciked does not equal to computer roll')
-                waitForPlayerClick();
-            }
-        })
-    }
-}
-
-function startComputerRoll(dataType) {
-    console.log('computer roll has started')
+function rollComputersDice(dataType){
     let cnum1 = Math.floor(Math.random() * 6 + 1);
     let cnum2 = Math.floor(Math.random() * 6 + 1);
-    console.log('numbers have been created')
-    let playerValue = parseInt(document.getElementById('player-roll').textContent);
-    let playGame2 = (dataType === 'computer-roll');
+    console.log(dataType);
 
-    if (playGame2) {
-        let computerSum = caluclateDiceValue(cnum1, cnum2);
-        let playerSum = parseInt(document.getElementById('player-roll').textContent);
+    let commandText = document.getElementById('command-text').textContent;
+    console.log(commandText);
+    let computerRoll = (dataType === 'computer-roll') && (commandText === 'Roll the Computers dice');
 
-        changeCommandText(`Computers first dice was ${cnum1} and Computers second dice was ${cnum2}`);
-        changeComputerRoll(computerSum);
-        console.log(computerSum, playerSum);
-        compareDiceValues(computerSum, playerSum);
+    if (computerRoll) {
+        changeComputerScore(cnum1, cnum2);
     } else{
-        console.log('data-type is not equal to computer roll and no games have been played')
+        console.log('button clicked does not equal player roll or command text does not equal Roll your dice!')
     }
 }
+function changeComputerScore(num1, num2) {
+    let csum = caluclateDiceValue(num1, num2);
+
+    changeCommandText(`Computers first dice was ${num1} and Computers second dice was ${num2}`);
+    changeComputerRoll(csum);
+}
+
+
 
 function caluclateDiceValue(num1, num2) {
     let sum = num1 + num2;
